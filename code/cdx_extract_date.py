1e14a0285c527d8aa36cc0a62d51313a535042ca
Mark Phillips <vphill@gmail.com>
2016-05-18 14:29:29 -0500

    Adding first scipt mainly as an example

---
+++ code/cdx_extract_date.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import fileinput
+from datetime import datetime
+
+
+for line in fileinput.input():
+    line = line.rstrip()
+
+    # This condition removes CDX header lines
+    if line[0] is ' ':
+        continue
+
+    # Extract just the timestamp from line
+    timestamp = line.split(' ', 2)[1]
+
+    # Datetiem format in CDX is  20121125005312
+    date_object = datetime.strptime(timestamp, '%Y%m%d%H%M%S')
+
+    print(date_object.strftime('%Y-%m-%d %H:%M:%S'))
